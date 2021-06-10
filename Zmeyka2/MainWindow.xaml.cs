522ff865756915d2f46e484f378b1a4af2a9d1fc
Дмитрий Михеев <234.dimon1@gmail.com>
2021-06-10 18:50:54 +1000

    123

--- Zmeyka2/MainWindow.xaml.cs
+++ Zmeyka2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Zmeyka2
 {
@@ -20,9 +21,39 @@ namespace Zmeyka2
     /// </summary>
     public partial class MainWindow : Window
     {
+        int _elementSize = 10;
+        DispatcherTimer _gameTimer;
         public MainWindow()
         {
             InitializeComponent();
+            InstalizeTimer();
+            DrawGame();
+        }
+
+        private void DrawGame()
+        {
+            var numberOfColumns = this.Width / _elementSize;
+            var numberOfRows = this.Height / _elementSize;
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                Lint line = new Line();
+                line.Stroke = Brushes.Black;
+
+            }
+        }
+
+        public void InstalizeTimer()
+        {
+            _gameTimer = new DispatcherTimer();
+            _gameTimer.Interval = TimeSpan.FromSeconds(0.5);
+            _gameTimer.Tick += MainGameLoop;
+            _gameTimer.Start();
+        }
+
+        private void MainGameLoop(object sender, EventArgs e)
+        {
+
         }
     }
 }
